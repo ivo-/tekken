186b22d5df7979486d689f68bea407a25148003d
ivo- <ivailo.nikolaev.hristov@gmail.com>
2014-04-26 01:26:45 +0300

    Add some ui shits

--- src/tekken/core.cljs
+++ src/tekken/core.cljs
@@ -1,3 +1,93 @@
-(ns tekken.core)
+(ns tekken.core
+    (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+    (:require  [tekken.util :as util]
+               [om.core :as om :include-macros true]
+               [om.dom :as dom :include-macros true]
+               [cljs.core.async :refer [put! <! >! chan map>]]))
 
-(js/alert "Hello, CLJS!")
+(defonce data
+  (atom {}))
+
+(defn editor
+  [app owner {:keys [ch]}]
+  (reify
+    om/IInitState
+    (init-state
+      [_]
+      {:text ""
+       :onChange
+       (fn [_]
+         (let [value (->> (om/get-node owner "text")
+                          (.-value))]
+           (om/set-state! owner :text value)
+           (put! ch value)))})
+
+    om/IRenderState
+    (render-state
+      [_ {:keys [text onChange]}]
+      (dom/form
+        #js {:action "javascript: void(0);"}
+        (dom/textarea
+          #js {:type "text"
+               :ref "text"
+               :rows "20"
+               :cols "30"
+               :value text
+               :onChange onChange})))))
+
+(defn viewer
+  "Test viewer component."
+  [app owner {:keys [ch]}]
+  (reify
+    om/IInitState
+    (init-state
+      [_]
+      {:markdown ""})
+
+    om/IWillMount
+    (will-mount
+      [_]
+     (go-loop
+      []
+      (let [v (<! ch)]
+        (om/set-state! owner :markdown v)
+        (recur))))
+
+    om/IRenderState
+    (render-state
+      [_ {:keys [markdown]}]
+      (dom/section
+       (clj->js {:className "viewer"
+;;                  :dangerouslySetInnerHTML {:__html (util/md->html markdown)}
+                 })
+       (pr-str (util/md->edn markdown))))))
+
+(defn home-ui
+  "Home page ui."
+  [app owner]
+  (reify
+    om/IInitState
+    (init-state
+     [_]
+     {:ch (chan)})
+
+    om/IRenderState
+    (render-state
+     [_ {:keys [ch]}]
+     (dom/section
+      #js {:className "home"}
+      (om/build editor app {:opts {:ch ch}})
+      (om/build viewer app {:opts {:ch ch}})))))
+
+(defn tekken
+  "The big boss that builds all the components together."
+  []
+  (om/root
+    (fn [app owner]
+      (dom/div
+        nil
+        (om/build home-ui app)))
+    data
+    {:target (. js/document (getElementById "main"))}))
+
+(tekken)
---
+++ src/tekken/util.cljs
@@ -0,0 +1,62 @@
+(ns tekken.util
+    (:require [cljs.core.async :refer [chan put!]]))
+
+(defn $
+  [x]
+  (if (string? x)
+    (js/document.querySelector x)
+    x))
+
+(defn html->canvas
+  [node]
+  (let [node ($ node)
+        ch (chan)])
+  (js/html2canvas
+   node
+   (fn [canvas]
+     (put! ch canvas))))
+
+(defn canvas->pdf
+  [cvs]
+  (let [data-url (. cvs toDataURL "image/jpeg")
+        pdfdoc  (jsPDF. "p" "mm" "a4")]
+    (. pdfdoc addImage data-url 0 0 210 297)
+    (. pdfdoc output "datauristring")))
+
+(defn md->html
+  [s]
+  (. js/Markdown toHTML s))
+
+(defn md->edn
+  [s]
+  (let [parse
+        (fn [data]
+          {:test data
+           :ansers
+           (->> data
+                (filter #(= (first %) "ul"))
+                (first)
+                (rest)
+                (map #(vector (second %) false)))})]
+    (->> s
+         (. js/Markdown toHTMLTree)
+         (js->clj)
+         (rest)
+         (partition-by #(= ["hr"] %))
+         (remove #(= [["hr"]] %))
+         (map parse))))
+
+(md->edn "
+Напишете някви лайна?
+
+```
+(def a 10)
+```
+
+- a
+- b
++ c
+- d
+
+---
+")
