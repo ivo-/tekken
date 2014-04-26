a67388b95951ebb71303a11feeed7e67c10ce118
popovm <popov.marto@gmail.com>
2014-04-27 02:51:15 +0300

    Add sample Sinatra file uploader

---
+++ resources/patternRecognition/fileUploader.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'haml'
+
+get "/upload" do
+  haml :upload
+end
+
+post "/upload" do
+  Process.fork do
+    # exec("./recognizer #{params['testfile'][:filename]} count_test 4 5 ./result.txt")
+    exec("cat #{params['testfile'][:filename]}")
+  end
+  return "The test was successfully checked!"
+end
+
