3e5f61f7b45f74ae5d0cef9c51f9be06bd4a5b7a
popovm <popov.marto@gmail.com>
2014-04-27 05:09:33 +0300

    Return json from test's results

--- resources/patternRecognition/fileUploader.rb
+++ resources/patternRecognition/fileUploader.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'haml'
+require 'json'
 
 get "/upload" do
   haml :upload
@@ -7,10 +8,26 @@ end
 
 post "/upload" do
   Process.fork do
-    # exec("./recognizer #{params['testfile'][:filename]} count_test 4 5 ./result.txt")
+    # exec("./recognizer #{params['testfile'][:filename]} count_test 4 5 ./results.txt")
     exec("cat #{params['testfile'][:filename]}")
   end
   Process.wait()
-  return "The test was successfully checked!"
+  return ResultParser.perform()
+end
+
+class ResultParser
+  def self.perform
+    answer_list = {}
+    File.open('./results.txt') do |results|
+      answer_list[:counts] = results.gets.chop
+      answer_list[:fn] = results.gets.chop
+      while line = results.gets
+        question = line.match(/.*-/).to_s.chop
+        answer = line.match(/-./).to_s[1]
+        answer_list[question] = answer
+      end
+    end
+    JSON.generate(answer_list)
+  end
 end
 
